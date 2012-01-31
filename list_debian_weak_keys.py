2bc9df94d2578cd15176940928d31c7b2a7e7013
Ondrej Mikle <ondrej.mikle@nic.cz>
2012-01-31 15:31:04 +0100

    Added debian-weak-key testing with adding few leading zeros as well

--- list_debian_weak_keys.py
+++ list_debian_weak_keys.py
@@ -16,12 +16,18 @@ keys_file = file(sys.argv[1])
 for line in keys_file:
     domain, key_type, exponent, modulus = line.rstrip().split(" ")
     
-    mod_to_hash = "Modulus="+modulus.lstrip("0").upper()+"\n"
-    mod_hash = sha1(mod_to_hash).hexdigest()
-    to_check = mod_hash[20:]
+    modulus = modulus.lstrip("0").upper() #we shouldn't have leading zeros, but just in case
 
-    #let's just check both weak1024 and weak2048 blacklists without worrying
-    #about the modulus length
-    for blacklist in [weak1024, weak2048]:
-        if to_check in blacklist:
-            print "Domain %s found with modulus %s" % (domain, modulus)
+    #how many zeros does openssl print with "-modulus"? Let's try it with up to 
+    #three - one for having even number of hex digits, two more to check with
+    #leading zero byte
+    for zeros in range(4):
+        mod_to_hash = "Modulus="+("0"*zeros)+modulus+"\n"
+        mod_hash = sha1(mod_to_hash).hexdigest()
+        to_check = mod_hash[20:]
+
+        #let's just check both weak1024 and weak2048 blacklists without worrying
+        #about the modulus length
+        for blacklist in [weak1024, weak2048]:
+            if to_check in blacklist:
+                print "Domain %s found with modulus %s" % (domain, modulus)
